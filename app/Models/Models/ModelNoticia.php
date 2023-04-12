cfc154c21888c99bc6a1ddb909211ec27c199716
jcleston <janes.cleston.silva@gmail.com>
2023-04-12 17:35:48 -0300

    feat: add Migration and Model of Noticia in project

---
+++ app/Models/Models/ModelNoticia.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelNoticia extends Model
+{
+    //use HasFactory;
+    protected $table='noticias';
+}
